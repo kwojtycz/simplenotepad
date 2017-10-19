3ebba4ba2ef7f49b1e9e287a2bf55caba44cc805
kwojtycz <wojtyczakonrad@gmail.com>
2017-10-19 17:35:17 +0200

    work in process

--- src/simplenotepad/Menu.java
+++ src/simplenotepad/Menu.java
@@ -4,30 +4,44 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 public class Menu {
-    public static void main(String[] args) throws IOException, StringIndexOutOfBoundsException {
-        System.out.println("This program helps you to manage short text files known as notes \nWhat would you like to do?");
-        System.out.println("Choose 1 to create/modify notes... \n or 2 to exit the program");
+    public static void main(String[] args) throws IOException {
 
         Scanner input = new Scanner(System.in);
-        int activity = input.nextInt();
+        input.useDelimiter("\n");
+        int activity =0;
+        System.out.println("This program helps you to manage short text files known as notes");
+        while(activity!=3){
+            System.out.println("What would you like to do?");
+            System.out.println("Choose 1 to create, 2 to modify notes... \n or 3 to exit the program");
 
-        switch (activity) {
-            case 1:
-                System.out.println("Write name of your note:");
-                String name = input.next();
-                noteName noteName = new noteName(name);
-                File noteFile = new File(noteName.noteName);
-                noteFile.createNewFile();
-                System.out.println("Write your text");
-                FileWriter writer = new FileWriter(noteFile);
-                String note = input.next();
-                writer.write(note);
-                writer.flush();
-                writer.close();
+            activity = input.nextInt();
 
-                break;
-            case 2:
-                break;
-        }
+
+                switch (activity) {
+                    case 1:
+                    System.out.println("Write name of your note:");
+                    String name = input.next();
+                    noteName noteName = new noteName(name);
+                    File noteFile = new File(noteName.noteName);
+                    noteFile.createNewFile();
+                    System.out.println("Write your text");
+                    FileWriter writer = new FileWriter(noteFile);
+                    String text = input.next();
+                    System.out.println(text);
+                    writer.write(text);
+                    writer.flush();
+                    writer.close();
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    
+                }
+
+            }
+
+
     }
 }
--- src/simplenotepad/noteName.java
+++ src/simplenotepad/noteName.java
@@ -1,13 +1,16 @@
 package simplenotepad;
 
 public class noteName {
-	public String noteName;
+    public String noteName;
 
-	public noteName(String noteName){
-		this.noteName = noteName;
-	}
+    public noteName(String noteName){
+        this.noteName = noteName;
+    }
 
-	public String getnoteName(){
-		return noteName;
-	}
+    public String getnoteName(){
+        return noteName;
+    }
+    public void createNote(){
+
+    }
 }
